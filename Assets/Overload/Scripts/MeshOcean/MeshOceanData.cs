977967a9c6465a85a636ada45f604237c5a984e9
alpcihan <alpcihan.ac@gmail.com>
2024-01-23 12:56:02 +0100

    update folder structure

--- Assets/Overload/Scripts/MeshOcean/MeshOceanData.cs
+++ Assets/Overload/Scripts/MeshOcean/MeshOceanData.cs
@@ -32,15 +32,4 @@ namespace overload
         public Material material;
     };
 
-    public struct InstanceData
-    {
-        public Matrix4x4 Matrix;
-        public Matrix4x4 MatrixInverse;
-
-        public static int Size()
-        {
-            return sizeof(float) * 4 * 4
-                 + sizeof(float) * 4 * 4;
-        }
-    }
 }
